45437f749e73b44a6978dee924ecee2246913abf
MANOJ.V_CSE <160569397+Manojvcse@users.noreply.github.com>
2024-07-20 11:33:18 +0530

    Add files via upload
    This is code for Light Intensity Measurement 
    sensor - LDR
    board-NodeMCU-8266

---
+++ Light_Intensity_Measure/Light_Intensity_Measure.ino
@@ -0,0 +1,48 @@
+#define BLYNK_TEMPLATE_ID "TMPL3EN8xyE5p"
+#define BLYNK_TEMPLATE_NAME "Light Intensity Measure"
+#define BLYNK_AUTH_TOKEN "q4CLpHHcb7MKdMHrFoRkM5BBeX0uDrmz"
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#define led D1
+#define sensor A0
+char auth[] = "q4CLpHHcb7MKdMHrFoRkM5BBeX0uDrmz";
+char ssid[] = "Mi 11X";
+char pass[] = "kavin@349";
+
+BlynkTimer timer;
+
+
+void setup()
+{
+  // Debug console
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(500L, sendSensor);
+
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
+
+void sendSensor()
+{
+  int LDR = analogRead(sensor);
+    if(LDR <150)
+  {
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+   
+  }
+
+  Blynk.virtualWrite(V5, LDR);
+
+}
